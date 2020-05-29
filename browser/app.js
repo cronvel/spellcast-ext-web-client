ea87acd6efecfff5bce9de4ff96838890a4b2843
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2020-05-29 21:01:29 +0200

    wip

--- browser/app.js
+++ browser/app.js
@@ -3068,6 +3068,8 @@ UI.prototype.initBus = function() {
 
 	this.bus.on( 'defineAnimation' , UI.defineAnimation.bind( this ) ) ;
 
+	this.bus.on( 'createGScene' , UI.createGScene.bind( this ) , { async: true } ) ;
+
 	this.bus.on( 'showSprite' , UI.showSprite.bind( this ) , { async: true } ) ;
 	this.bus.on( 'updateSprite' , UI.updateSprite.bind( this ) , { async: true } ) ;
 	this.bus.on( 'animateSprite' , UI.animateSprite.bind( this ) , { async: true } ) ;
@@ -3525,6 +3527,13 @@ UI.image = function( data ) {
 
 UI.defineAnimation = function( id , data ) {
 	this.dom.defineAnimation( id , data ) ;
+} ;
+
+
+
+UI.createGScene = function( id , data , callback ) {
+	console.warn( "createGScene" , id , data ) ;
+	callback() ;
 } ;
 
 
@@ -15028,7 +15037,7 @@ camel.camelCaseToDashed = ( str ) => camel.camelCaseToSeparated( str , '-' ) ;
 arguments[4][28][0].apply(exports,arguments)
 },{"dup":28}],46:[function(require,module,exports){
 module.exports={
-  "_from": "svg-kit@0.3.0",
+  "_from": "svg-kit@^0.3.0",
   "_id": "svg-kit@0.3.0",
   "_inBundle": false,
   "_integrity": "sha512-+lqQ8WQp8UD1BlNBeVOawBKpXCBCqdwnEfRiWxG7vI3NBmZ9CBPN/eMmMt2OpJRU8UcZUOrarAjiZV3dZsqWtA==",
@@ -15037,22 +15046,21 @@ module.exports={
     "@cronvel/xmldom": "0.1.31"
   },
   "_requested": {
-    "type": "version",
+    "type": "range",
     "registry": true,
-    "raw": "svg-kit@0.3.0",
+    "raw": "svg-kit@^0.3.0",
     "name": "svg-kit",
     "escapedName": "svg-kit",
-    "rawSpec": "0.3.0",
+    "rawSpec": "^0.3.0",
     "saveSpec": null,
-    "fetchSpec": "0.3.0"
+    "fetchSpec": "^0.3.0"
   },
   "_requiredBy": [
-    "#USER",
     "/"
   ],
   "_resolved": "https://registry.npmjs.org/svg-kit/-/svg-kit-0.3.0.tgz",
   "_shasum": "a53aadb7152cf7374e2a791b9d45b7cc6d0fe25d",
-  "_spec": "svg-kit@0.3.0",
+  "_spec": "svg-kit@^0.3.0",
   "_where": "/home/cedric/inside/github/spellcast-ext-web-client",
   "author": {
     "name": "Cédric Ronvel"
